51786402127cf95bb50a69af4d38e751002a7865
Nicholas Angleton <nick.angleton@gmail.com>
2022-04-04 17:05:37 -0400

    produce odd shading effect

--- src/scripts/etching.js
+++ src/scripts/etching.js
@@ -1,62 +1,65 @@
-export class Etching{
+export class Etching {
   constructor(bitMap){
-    this.unit = 8;
-    this.height = bitMap.height - (bitMap.height % this.unit);
-    this.width = bitMap.width - (bitMap.width % this.unit);
+    this.unit = 5;
+    this.area = this.unit * this.unit;
+
+    this.width = bitMap.width;
+    this.hor = Math.floor(this.width / this.unit);
+    this.horEdge = this.width % this.unit;
+
+    this.height = bitMap.height;
+    this.vert = Math.floor(this.height / this.unit);
+    this.vertEdge = this.height % this.unit;
 
     this.hiddenCanvas = new OffscreenCanvas(this.height, this.width);
     this.hiddenCtx = this.hiddenCanvas.getContext('2d');
-    this.hiddenCtx.drawImage(bitMap, 0, 0, this.width, this.height);
-
+    this.hiddenCtx.drawImage(bitMap, 0, 0);
+    
     this.canvas = document.getElementById('canvas');
     this.canvas.width = this.width;
     this.canvas.height = this.height;
-    this.ctx = this.canvas.getContext('2d');
-
-    this.newData = this.ctx.createImageData(this.width, this.height);
+    this.ctx = canvas.getContext('2d');
 
     this.data = this.hiddenCtx.getImageData(0, 0, this.width, this.height);
+    this.newData = this.ctx.createImageData(this.data);
 
-    this.area = this.unit * this.unit;
-    this.pixels = this.data.data.length / 4;
-    this.countOfCells = this.pixels / this.area;
-    this.etchify = this.etchify.bind(this);
     this.etchify();
+    // this.ctx.drawImage(bitMap, 0, 0);
   }
 
   etchify(){
-    for (let cell = 0; cell < this.countOfCells; cell++) {
-      let acc = 0;
-      let row = Math.floor(cell / (this.width / this.unit))
-      let col = (cell % (this.width / this.unit))
-      for (let pixel = 0; pixel < this.area; pixel++) {
-        let dRow = row + Math.floor(pixel / this.unit);
-        let dCol = col + (pixel % this.unit);
-        let num = 4 * ((dRow * this.width) + dCol);
-        acc += (this.data.data[num] + this.data.data[num + 1] + this.data.data[num + 2]);
+    let acc = 0;
+    for (let row = 0; row < this.hor + 500; row++) {
+      for (let col = 0; col < this.vert + 500; col++) {
+        acc = 0;
+        for (let i = 0; i < this.area; i++){
+          let prev_rows = 4 * (((row * this.unit) + Math.floor(i / this.unit)) * this.width);
+          let prev_col = 4 * ((col * this.unit) + (i % this.unit));
+          acc += this.data.data[prev_rows + prev_col];
+          acc += this.data.data[prev_rows + prev_col + 1];
+          acc += this.data.data[prev_rows + prev_col + 2];
+          // console.log(`row: ${row}, col: ${col}, prev_rows: ${prev_rows}, prev_col: ${prev_col}`);
+        }
+        this.drawCell(row, col, acc);
       }
-      this.drawCell(cell, acc);
     }
-    debugger
-    console.log(this.newData);
+    console.log(this.newData)
     this.ctx.putImageData(this.newData, 0, 0);
   }
 
-  drawCell(cell, acc) {
-    if (cell % 1000 === 0) console.log(cell);
-    let val = (acc / (this.area * 3));
-    let row = Math.floor(cell / (this.width / this.unit));
-    let col = (cell % (this.width / this.unit));
+  drawCell(row, col, acc){
+    let val = Math.floor(acc / (3 * this.area));
 
-    for (let pixel = 0; pixel < this.area; pixel++) {
-      let dRow = row + Math.floor(pixel / this.unit);
-      let dCol = col + (pixel % this.unit);
-      let num = 4 * ((dRow * this.width) + dCol);
-      this.newData.data[num] = val;
-      this.newData.data[num + 1] = val;
-      this.newData.data[num + 2] = val;
-      this.newData.data[num + 3] = 255;
+    for (let i = 0; i < this.area; i++) {
+      let prev_rows = 4 * (((row * this.unit) + Math.floor(i / this.unit)) * this.width);
+      let prev_col = 4 * ((col * this.unit) + (i % this.unit));
+      // console.log(`val: ${val}, row: ${row}, col: ${col}, prev_rows: ${prev_rows}, prev_col: ${prev_col}`);
+      this.newData.data[prev_rows + prev_col] = val;
+      this.newData.data[prev_rows + prev_col + 1] = val;
+      this.newData.data[prev_rows + prev_col + 2] = val;
+      this.newData.data[prev_rows + prev_col + 3] = 255;
     }
   }
+
 
 }
