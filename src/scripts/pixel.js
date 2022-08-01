acac0c7dcf9e853d090ba8d7494f1d3e68b17bb5
Nicholas Angleton <nick.angleton@gmail.com>
2022-08-01 01:57:26 -0400

    add gitattributes

--- src/scripts/pixel.js
+++ src/scripts/pixel.js
@@ -1,29 +1,29 @@
 export class Pixel {
 
-    constructor(colors, darkness) {
-        this.colors = colors;
-        this.darkness = darkness;
-        this.neighbors = [];
-        this.lineBool = false;
-        this.checked = false;
-    }
+  constructor(colors, darkness) {
+    this.colors = colors;
+    this.darkness = darkness;
+    this.neighbors = [];
+    this.lineBool = false;
+    this.checked = false;
+  }
 
-    line(colorDiffThreshold, percentOfPixels) {
-        let weightedNumberOfLine = 0.0;
-        let hypotheticalMax = 0.0;
-        this.neighbors.forEach((neighborValues) => {
-            let colorDifference = neighborValues[0];
-            let weight = neighborValues[1];
-            hypotheticalMax += weight;
-            if (colorDifference >= colorDiffThreshold) weightedNumberOfLine += weight;
-        });
-        this.lineBool = Boolean((weightedNumberOfLine / hypotheticalMax) > percentOfPixels);
-        return this.lineBool;
-    }
+  line(colorDiffThreshold, percentOfPixels) {
+    let weightedNumberOfLine = 0.0;
+    let hypotheticalMax = 0.0;
+    this.neighbors.forEach(neighborValues => {
+      let colorDifference = neighborValues[0];
+      let weight = neighborValues[1];
+      hypotheticalMax += weight;
+      if (colorDifference >= colorDiffThreshold) weightedNumberOfLine += weight;
+    });
+    this.lineBool = Boolean((weightedNumberOfLine / hypotheticalMax) > percentOfPixels);
+    return this.lineBool;
+  }
 
-    static addColorDifference(pixel1, pixel2, colorDifference, weight) {
-        pixel1.neighbors.push([colorDifference, weight]);
-        pixel2.neighbors.push([colorDifference, weight]);
-    }
+  static addColorDifference(pixel1, pixel2, colorDifference, weight) {
+    pixel1.neighbors.push([colorDifference, weight]);
+    pixel2.neighbors.push([colorDifference, weight]);
+  }
 
 }
